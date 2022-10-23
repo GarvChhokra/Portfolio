fe628030470fe86adc68d4c6c85fc75562f82434
Garv <garvchhokra@gmail.com>
2022-10-23 03:26:57 -0400

    commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // URI: "mongodb://127.0.0.1:27017/business_contacts",
+    URI: "mongodb+srv://Garv:garv@mongodbserver.c6cbr9b.mongodb.net/business_contacts?retryWrites=true&w=majority",
+    Secret: "SomeSecret",
+};
---
+++ server/routes/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+let indexController = require("../controllers/index");
+
+/* GET home page. */
+router.get("/", indexController.displayHomePage);
+
+/* GET home page. */
+router.get("/home", indexController.displayHomePage);
+
+/* GET About Us page. */
+router.get("/aboutme", indexController.displayAboutPage);
+
+/* GET Products page. */
+router.get("/projects", indexController.displayProductsPage);
+
+/* GET Services page. */
+router.get("/services", indexController.displayServicesPage);
+
+/* GET Contact Us page. */
+router.get("/contactme", indexController.displayContactPage);
+router.get("/busContact", indexController.displayBusPage);
+///Extra Ends
+// Displaying Login Page
+router.get("/logins", indexController.displayLoginPage);
+/* POST Route for processing the Login page */
+router.post("/logins", indexController.processLoginPage);
+
+/* GET Route for displaying the Register page */
+router.get("/register", indexController.displayRegisterPage);
+
+/* POST Route for processing the Register page */
+router.post("/register", indexController.processRegisterPage);
+
+/* GET to perform UserLogout */
+router.get("/logout", indexController.performLogout);
+module.exports = router;
